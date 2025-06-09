d27a4ce4697454351861341e5e9afe2a2ad7a848
JuanCruzEmmanuel <juancruznoya@mi.unc.edu.ar>
2025-06-09 12:50:31 -0300

    Se agrega una mejor visualizacion de la tabla de protocolos

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ from GUI.log import LogWindow
 from CONTROLADORES.BBDD import SMVA_DB
 
 def main():
-    __VERSION__ = "1.2.1"
+    __VERSION__ = "1.2.2"
     database = SMVA_DB() #Cargo la base de datos
     app = QApplication(sys.argv)
 
